5cc49153888e7519c81e1d3506d48fcfba12397e
Dave Ford <dford215@gmail.com>
2013-02-23 10:53:28 -0500

    Add documentation and database setup file

---
+++ setupDB.sql
@@ -0,0 +1,22 @@
+/*
+user: image
+password: ImageOrganizer
+*/
+
+USE imageorganizerdb;
+
+CREATE TABLE IF NOT EXISTS Image_Directories(
+	ID			INT		AUTO_IMCREMENT		PRIMARY KEY,
+	Dir_Path	VARCHAR(100)	NOT NULL,
+);
+
+
+CREATE TABLE IF NOT EXISTS Image_Meta(
+	ID				int				auto_increment		primary key,
+	Name			varchar(25) 	NOT NULL,
+	Filename		varchar(29)		NOT NULL,
+	Description		varchar(500)	NOT NULL,
+	Location		INT				NOT NULL,
+	Insert_Date		DATETIME,
+	FOREIGN KEY 	(Location) 		REFERENCES 			Image_Directories(ID)
+);
